c445892365e65c31c64358d6549ede6063eb5366
Kunj <shahkunj42@gmail.com>
2022-08-19 17:54:38 -0400

    commit tree 9e2ad51a401e7fe6e28c738eaec0f0a604eadc57

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,43 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def breadth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+
+    result 
+  end
+
+  def depth_first_traversal(node)
+    result = []
+    nodes_to_visit = [node]
+  
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node[:value])
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+  
+    result
+  end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+
+    until nodes_to_visit.empty?
+      current = nodes_to_visit.pop
+      return current if current[:id] == id
+
+      nodes_to_visit = nodes_to_visit + current[:children]
+    end
+    nil
+  end
+  
 end
